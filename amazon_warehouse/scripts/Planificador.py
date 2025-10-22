8b580b9cc47310b40bb63056f1424795b4486d0d
CarlosC_strix <carlosadrian.cancela@rai.usc.es>
2025-10-22 16:44:34 +0000

    Avances en operadores, comprobar que funcionan bien, y que hacen un estado nuevo, para que no haga cosas raras de copiar referencia en vez de valores y cambie por atras.
    Funcion para dibujar mapita, usa numpy.

--- amazon_warehouse/scripts/Planificador.py
+++ amazon_warehouse/scripts/Planificador.py
@@ -1,4 +1,6 @@
 from typing import Any
+import numpy as np  # Se usa para copiar el entorno para el mapa.
+
 
 class Palet():
     def __init__(self,x_inicial: int,y_inicial:int ,ang_inicial:bool ,x_objetivo:int ,y_objetivo:int ,ang_objetivo:bool) -> None:
@@ -50,18 +52,158 @@ class Busqueda():
     def __init__(self,estado_inicial: "Estado",estado_objetivo: "Estado", entorno: "list[list]") -> None:
         # Mapeo de orientaciones
         self.movimientos = {
-            'N': (-1, 0),
-            'S': (1, 0),
+            'N': (1, 0),
+            'S': (-1, 0),
             'E': (0, 1),
             'O': (0, -1)
         }
+        self.rotar_izquierda = {'N': 'O', 'O': 'S', 'S': 'E', 'E': 'N'}
+        self.rotar_derecha = {'N': 'E', 'E': 'S', 'S': 'O', 'O': 'N'}
+
+        self.estado_ini = estado_inicial
+        self.estado_final = estado_objetivo
+        self.estado_actual = estado_inicial
+
+
         self.filas = len(entorno)
         self.columnas = len(entorno[0])
 
         self.entorno = entorno
 
+
         return None
     
+
+    def levantar_bajar(self, estado: Estado):
+        """
+        Tiene que estar debajo de un palet para poder usar esto,
+        si no, no sube o baja.
+
+        Va a invertir el estado que tenga.
+        """
+        
+        
+        cord_robot_x: int  = estado.Robot_x
+        cord_robot_y: int = estado.Robot_y
+        robot_angulo: str = estado.Robot_orientacion
+        rob_activado: bool = estado.Robot_activado
+        lis_estanterias: "list[Palet]" = estado.Lista_estanterias.copy()
+
+        
+        for palet in lis_estanterias:
+            if cord_robot_x == palet.pos_x and cord_robot_y == palet.pos_y:
+                rob_activado = not(rob_activado)
+
+                estado_nuevo: Estado = Estado(cord_robot_x,cord_robot_y,robot_angulo,rob_activado,lis_estanterias)
+                return estado_nuevo
+
+        # Si no ha encontrado un palet encima del robot, no se devuelve estado
+        return None
+
+    def girar_izq(self, estado: Estado):
+        """
+            Gira el robot a la izquierda
+            Si no tiene palet no hay requisitos para girar
+
+            Si lo hay, no puede haber nada alrededor de la posicion del robot
+        
+        """
+
+        cord_robot_x: int  = estado.Robot_x
+        cord_robot_y: int = estado.Robot_y
+        robot_angulo: str = estado.Robot_orientacion
+        rob_activado: bool = estado.Robot_activado
+        lis_estanterias: "list[Palet]" = estado.Lista_estanterias.copy()
+
+
+        if rob_activado == 0:
+            robot_angulo = self.rotar_izquierda[robot_angulo]
+
+            estado_nuevo: Estado = Estado(cord_robot_x,cord_robot_y,robot_angulo,rob_activado,lis_estanterias)
+            return estado_nuevo
+
+        else: 
+
+            # Si lleva palet hay que hacer mas comprobaciones
+            # Que no este cerca de algun obstaculo o pared
+            for ancho in range(-1,1):
+                for alto in range(-1,1):
+                    if self.entorno[cord_robot_x +ancho][cord_robot_y + alto]:
+                        return None
+
+
+            for palet in lis_estanterias:
+
+                # Gira el palet si es el que esta en la posicion del robot
+                if palet.pos_x == cord_robot_x and palet.pos_y == cord_robot_y:
+                    palet.ang_actual = not(palet.ang_actual)
+
+                else:
+
+                    # Si no es el palet que lleva
+                    # Mira alrededor del otro palet, no se puede girar ahi.
+                    for ancho in range(-1,1):
+                        for alto in range(-1,1):
+                            if cord_robot_x == palet.pos_x +ancho and cord_robot_y == palet.pos_y + alto:
+                                return None
+            estado_nuevo: Estado = Estado(cord_robot_x,cord_robot_y,robot_angulo,rob_activado,lis_estanterias)
+            return estado_nuevo
+
+
+    def girar_der(self, estado: Estado):
+        """
+            Gira el robot a la izquierda
+            Si no tiene palet no hay requisitos para girar
+
+            Si lo hay, no puede haber nada alrededor de la posicion del robot
+        
+        """
+        cord_robot_x: int  = estado.Robot_x
+        cord_robot_y: int = estado.Robot_y
+        robot_angulo: str = estado.Robot_orientacion
+        rob_activado: bool = estado.Robot_activado
+        lis_estanterias: "list[Palet]" = estado.Lista_estanterias.copy()
+
+
+
+        if rob_activado == 0:
+            robot_angulo = self.rotar_derecha[robot_angulo]
+
+
+            estado_nuevo: Estado = Estado(cord_robot_x,cord_robot_y,robot_angulo,rob_activado,lis_estanterias)
+
+            return estado_nuevo
+
+        else: 
+            # Si lleva palet hay que hacer mas comprobaciones
+            
+
+            # Que no este cerca de algun obstaculo o pared
+            for ancho in range(-1,1):
+                for alto in range(-1,1):
+                    if self.entorno[cord_robot_x +ancho][cord_robot_y + alto]:
+                        return None
+
+
+            for palet in lis_estanterias:
+
+                # Gira el palet si es el que esta en la posicion del robot
+                if palet.pos_x == cord_robot_x and palet.pos_y == cord_robot_y:
+                    palet.ang_actual = not(palet.ang_actual)
+
+                else:
+
+                    # Si no es el palet que lleva
+                    # Mira alrededor del otro palet, no se puede girar ahi.
+                    for ancho in range(-1,1):
+                        for alto in range(-1,1):
+                            if cord_robot_x == palet.pos_x +ancho and cord_robot_y == palet.pos_y + alto:
+                                return None
+                            
+
+            estado_nuevo: Estado = Estado(cord_robot_x,cord_robot_y,robot_angulo,rob_activado,lis_estanterias)
+            return estado_nuevo
+
 
     def avanzar(self, estado: Estado) -> Any:
 
@@ -90,12 +232,12 @@ class Busqueda():
                 if palet.ang_actual == 1: # Si el palet esta en vertical
                     # Al estar en vertical el robot no se puede mover 1 encima o debajo
                     # de la posicion del palet
-                    if palet.pos_x == Rx_n and (palet.pos_y == (Ry_n-1) or palet.pos_y == (Ry_n+1) ):
+                    if palet.pos_x == Rx_n and (palet.pos_y + 1 == Ry_n or palet.pos_y -1 == Ry_n ):
                         return None
                 else:
                     # Si esta horizontal, el robot no se puede poner 1 casilla por los lados
                     # en eje X,
-                    if palet.pos_y == Ry_n and (palet.pos_x == (Rx_n-1) or palet.pos_x == (Rx_n+1) ):
+                    if palet.pos_y == Ry_n and (palet.pos_x +1 == (Rx_n) or palet.pos_x -1  == Rx_n):
                         return None
 
             lista_palets_nueva = estado.Lista_estanterias.copy()
@@ -137,13 +279,19 @@ class Busqueda():
             if lleva_vertical == True:
 
                 # Que no choque con entorno el palet MIRAR SI ES REAL
-                if 0 > Rx_n > self.filas  or 0 > Ry_n + 1 > self.columnas or self.entorno[Rx_n][Ry_n]:
+                if 0 > Rx_n > self.filas  or 0 > Ry_n + 1 > self.columnas or self.entorno[Rx_n][Ry_n+1]:
                     return None
-                elif 0 > Rx_n > self.filas  or 0 > Ry_n - 1 > self.columnas or self.entorno[Rx_n][Ry_n]:
+                elif 0 > Rx_n > self.filas  or 0 > Ry_n - 1 > self.columnas or self.entorno[Rx_n][Ry_n-1]:
                     return None
 
 
                 for palet in lista_palets_quietos:
+
+                    # Que no se meta en otro palet
+                    if Rx_n == palet.pos_x and Ry_n == palet.pos_y:
+                        return None
+
+
                     if palet.ang_actual == 1: # Si el otro palet esta en vertical
 
                         # Al estar en vertical el robot no se puede mover 1 encima o debajo
@@ -158,7 +306,40 @@ class Busqueda():
                             for alto in range(-1,1):
                                 if Rx_n == palet.pos_x +ancho and Ry_n == palet.pos_y + alto:
                                     return None
+            else:
+                # Si lo lleva en horizontal
 
+
+                
+                # Que no choque con entorno el palet MIRAR SI ES REAL
+                if 0 > Rx_n+1 > self.filas  or 0 > Ry_n  > self.columnas or self.entorno[Rx_n+1][Ry_n]:
+                    return None
+                elif 0 > Rx_n-1 > self.filas  or 0 > Ry_n  > self.columnas or self.entorno[Rx_n-1][Ry_n]:
+                    return None
+
+
+                for palet in lista_palets_quietos:
+
+
+                     # Que no se meta en otro palet
+                    if Rx_n == palet.pos_x and Ry_n == palet.pos_y:
+                        return None
+                    
+
+                    if palet.ang_actual == 0: # Si el otro palet esta en horizontal
+
+                        # Al estar en horizontal el robot no se puede mover 1 der o izquierda
+                        # de la posicion del palet
+                        if palet.pos_y == Ry_n and (palet.pos_x-1 == (Rx_n) or palet.pos_x == (Rx_n) or palet.pos_x+1 == (Rx_n)):
+                            return None
+                        
+                    else:
+                       # Mira alrededor del otro palet, no se puede mover ahi.
+                        for ancho in range(-1,1):
+                            for alto in range(-1,1):
+                                if Rx_n == palet.pos_x +ancho and Ry_n == palet.pos_y + alto:
+                                    return None
+                
 
 
             #PENDIENTE DE SI LLEVA EL PALET EN HORIZONTAL
@@ -169,23 +350,89 @@ class Busqueda():
         return Estado(Rx_n,Ry_n,Rang,R_levan,Lista_palets=lista_palets_nueva) #type:ignore
 
 
-    def expandir(self, estado_original: Estado):
+    def expandir(self, estado_dado: Estado):
 
-        sucesores =[]
+        sucesores: "list[Estado]" =[]
+
+
+
 
         # Añadir cuando puede avanzar
+        """
+        Pendiente hacer que haga bastantes distancias,
+        hacer bucle  o algo asi.
+        
+        """
+        estado_nuevo = self.avanzar(estado_dado)
+        if estado_nuevo != None:
+            sucesores.append(estado_nuevo)
+
+
+
         # Cuando puede girar izq
+        estado_nuevo = self.girar_izq(estado=estado_dado)
+        if estado_nuevo != None:
+            sucesores.append(estado_nuevo)
+
         # Cuando puede girar der
-        # Cuando levanta o baja palet
+        estado_nuevo = self.girar_der(estado=estado_dado)
+        if estado_nuevo != None:
+            sucesores.append(estado_nuevo)
+
+
+        # levantar o bajar palet
+        estado_nuevo = self.levantar_bajar(estado=estado_dado)
+        if estado_nuevo != None:
+            sucesores.append(estado_nuevo)
+
+
+        # Imprimir los sucesores generados
+        print("Num estados: ", len(sucesores)); print(sucesores)
+        for s in sucesores:
+            if s != None:
+                print("Estado:")
+                self.imprimir(s,self.entorno)
+
+
 
 
         return sucesores
 
+
+    def imprimir(self,estado: Estado ,entorno) -> None:
+        
+        #mapa = [[0 for i in range(self.filas)] for j in range(self.columnas)]
+        mapa = np.copy(entorno)
+
+        if estado != None:
+            mapa[estado.Robot_x][estado.Robot_y] = ord(estado.Robot_orientacion)//10
+
+        #print("robot x: ",estado.Robot_x)
+        #print("robot y: ",estado.Robot_y)
+        #print("Orientacion: ",estado.Robot_orientacion)
+
+        for fila in mapa:
+            print(fila)
+
+        return None
+
 def main():
 
-    palet1 = Palet(0,0,True,3,3,True)
+    entorno = [
+        [0, 0, 0, 0,0],
+        [1, 1, 0, 1,0],
+        [0, 0, 0, 0,0],
+        [0, 1, 1, 0,0]
+    ]
 
-    print(palet1)
+    situacion1 = Estado(0,0,"E",False,[])
+
+    situacion_final = Estado(2,3,"N",False,[])
+
+    buscador = Busqueda(situacion1,situacion_final,entorno)
+
+
+    buscador.expandir(situacion1)
 
     return None
 
